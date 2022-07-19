4800e7e54ee8d257a0ee417ac1f70be1169270d1
Rafael Rodriguez Casanova <rafaelrodriguezcasanova@proton.me>
2022-07-18 21:00:58 -0400

    area acquired with some validation

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -2,13 +2,24 @@ package org.launchcode.java.studios.areaofacircle;
 import java.util.Scanner;
 
 public class Area {
+    public static Double GetAreaMethod (Double radius){
+        return radius*radius*3.14;
+
+    }
+
     public static void main(String[] args) {
         double radius;
+
         Scanner Input = new Scanner(System.in);
         System.out.println("Enter a radius to get the area of that circle");
 
-        radius = Input.nextDouble();
+        if (Input.hasNextDouble()) {
+            radius = Input.nextDouble();
+            System.out.println("The area of your circle: " + GetAreaMethod(radius));
+        } else {
+            System.out.println("Please type a number");
+        }
+
         Input.close();
-        System.out.println("The area of your circle: " + radius*radius*3.14);
     }
 }
